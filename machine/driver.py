e37466b34aa728c06d0fe622253d170744d3692a
Elliot Konetzni <ekonetzni@gmail.com>
2015-03-02 20:49:21 -0700

    initial stubbing

---
+++ machine/driver.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+
+class Driver(object):
+	"""
+	Driver is responsible for moving the machine. 
+	Whatever that means.
+	"""
+
+	def goTo(x=0,y=0):
+		"""
+		It literally just goes to where it's told
+		"""
+		return
+
+	def __init__(self):
+		"""
+		Do some initialization of the machine.
+		1. Zero the motors (Is that a thing?)
+		2. ???
+		3. Rest the combobulators.
+		4. Make art.
+		"""
+		return
---
+++ machine/navigator.py
@@ -0,0 +1,17 @@
+
+
+class Navigator(object):
+  """
+  Navigator provides the basic functionality necessary
+  for converting raw materials into a set of relatable coordinates.
+  """
+
+  def convertToPoint(rawMaterials):
+  	"""
+  	It shouldn't matter what rawMaterials contains, but it 
+  	probably does.
+  	"""
+    return
+
+  def __init__(self):
+    return
