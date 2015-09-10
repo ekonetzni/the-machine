8a4244a29374fab8183d01334492ec4b6510eb17
Elliot Konetzni <ekonetzni@gmail.com>
2015-09-09 20:30:48 -0600

    interlacing writes an image, but it doesn't look very good.

--- bin/run.py
+++ bin/run.py
@@ -6,6 +6,32 @@ import cv2
 import click
 import numpy as np
 
+def hilarity(a, b):
+  numRows = numColumns = 0
+  
+  numRows = min(len(a), len(b))
+  numColumns = min(len(a[0]), len(b[0]))
+  newImage = np.empty((numRows, numColumns, 3))
+
+  print "%d rows, %d columns" % (numRows, numColumns)
+
+  for row in range(numRows - 1):
+    for column in range(numColumns - 1):
+      if row % 2 == 0:
+        if column % 2 == 0:
+          newImage[row, column] = a[row, column]
+        else:
+          newImage[row, column] = b[row, column]
+      else:
+        if column % 2 == 0:
+          newImage[row, column] = b[row, column]
+        else:
+          newImage[row, column] = a[row, column]
+ 
+  return newImage
+
+
+
 def midlineDominate(image):
   """
   Takes a sample pixel from the midline of each row
@@ -48,8 +74,13 @@ def shift(image, amount):
 
   return image
 
-def _getDimensions(image):
-  return (len(image), len(image[0]))
+def _getDimensions(image, dimension="both"):
+  if dimension == "rows":
+    return len(image)
+  elif dimension == "columns":
+    return len(image[0])
+  else:
+    return (len(image), len(image[0]))
 
 
 @click.command()
@@ -74,6 +105,21 @@ def run(service, video, out):
   elif service == 'dominate':
     print "Color dominating"
     v.writeAllFrames(out, map(midlineDominate, v.readAllFrames(video)))
+  elif service == 'hilarious':
+    v1 = Videographer()
+    v1.video('../assets/chesapeake-1080.mp4')
+    v2 = Videographer()
+    v2.video('../assets/water.MOV')
+
+    i1 = v1.readNextFrame()
+    i2 = v2.readNextFrame()
+
+    img = hilarity(i1, i2)
+    v.writeImage(out, img)
+
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
   else:
     print len(image[0])
 
