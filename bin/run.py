8077f2dd9d30320abb384d49c1978853607789b2
Elliot Konetzni <ekonetzni@gmail.com>
2015-09-22 17:33:05 -0600

    moving code where it needs to be

--- bin/run.py
+++ bin/run.py
@@ -6,82 +6,7 @@ import cv2
 import click
 import numpy as np
 
-def hilarity(a, b):
-  numRows = numColumns = 0
-  
-  numRows = min(len(a), len(b))
-  numColumns = min(len(a[0]), len(b[0]))
-  newImage = np.empty((numRows, numColumns, 3))
-
-  print "%d rows, %d columns" % (numRows, numColumns)
-
-  for row in range(numRows - 1):
-    for column in range(numColumns - 1):
-      if row % 2 == 0:
-        if column % 2 == 0:
-          newImage[row, column] = a[row, column]
-        else:
-          newImage[row, column] = b[row, column]
-      else:
-        if column % 2 == 0:
-          newImage[row, column] = b[row, column]
-        else:
-          newImage[row, column] = a[row, column]
- 
-  return newImage
-
-
-
-def midlineDominate(image):
-  """
-  Takes a sample pixel from the midline of each row
-  makes the row that color
-  """
-  if not image is None:
-    numRows, numColumns = _getDimensions(image)
-
-    mid = numColumns / 2
-
-    for row in range(0, numRows - 1):
-      color = image[row, mid]
-      for column in range(0, numColumns - 1):
-        image[row, column] = color
-
-  return image
-
-def swapColumns(image):
-  numRows, numColumns = _getDimensions(image)
-
-  for row in range(0, numRows - 1):
-    if row % 2 == 0:
-      for column in range(0, numColumns - 1):
-        target = column + 5
-        if target > 639:
-          target = target - 639
-        image[row, column] = image[row, target]
-
-  return image
-
-def shift(image, amount):
-  numRows, numColumns = _getDimensions(image)
-
-  for row in range(0, numRows - 1):
-    target = row + amount 
-    if target > numRows - 1:
-      target = target - numRows - 1
-
-    image[row] = image[target]
-
-  return image
-
-def _getDimensions(image, dimension="both"):
-  if dimension == "rows":
-    return len(image)
-  elif dimension == "columns":
-    return len(image[0])
-  else:
-    return (len(image), len(image[0]))
-
+# NOTE: It seems values are actually stored as BGR.
 
 @click.command()
 @click.option('--service', 
@@ -93,18 +18,10 @@ def _getDimensions(image, dimension="both"):
 def run(service, video, out):
   v = Videographer()
   v.video(video)
-  
-  # We're just going to work on one image at a time for now.
-  image = v.readNextFrame()
 
-  if service == 'swap-columns':
-    v.writeImage('%s/swapColumns.jpg' % out, swapColumns(image))
-  elif service == 'shift':
-    print "Shifting stuff"
-    v.writeImage('%s/shifted.jpg' % out, swapStuff(image))
-  elif service == 'dominate':
-    print "Color dominating"
-    v.writeAllFrames(out, map(midlineDominate, v.readAllFrames(video)))
+  if service == 'midline-h':
+    print "Using midline horizontal processing..."
+    v.writeAllFrames(out, map(v.midlineHorizontal, v.readAllFrames(video)))
   elif service == 'hilarious':
     v1 = Videographer()
     v1.video('../assets/chesapeake-1080.mp4')
@@ -121,7 +38,8 @@ def run(service, video, out):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
   else:
-    print len(image[0])
+    #groupBySimilarity(image)
+    #_findReddest(image)
 
   #cv2.imshow('image', image)
   #cv2.waitKey(0)
--- machine/consultants/videographer.py
+++ machine/consultants/videographer.py
@@ -78,6 +78,91 @@ class Videographer(Consultant):
 
     v.release()
 
+  # MODIFICATION METHODS
+
+  def midlineHorizontal(self, image):
+    """
+    Takes a sample pixel from the midline of each row
+    makes the row that color
+    """
+    if not image is None:
+      numRows, numColumns = self._getDimensions(image)
+
+      mid = numColumns / 2
+
+      for row in range(numRows - 1):
+        color = image[row, mid]
+        for column in range(numColumns - 1):
+          image[row, column] = color
+
+    return image
+
+  def groupBySimilarity(self, image):
+    numRows, numColumns = self._getDimensions(image)
+    new = image
+    mid = numColumns / 2
+
+    for row in range(numRows - 1):
+      color = image[row, mid]
+      for column in range(numColumns - 1):
+        if _isSimilar(image[row, column], color, 10):
+          # What do I want to do here?
+          print image[row, column]
+          print "similar"
+          print color
+
+
+  def findReddest(self, image):
+    numRows, numColumns = self._getDimensions(image)
+    location = (0, 0)
+    highestR = 0
+
+    for row in range(numRows - 1):
+      for column in range(numColumns - 1):
+        blue, green, red = image[row, column]
+        total = red + green + blue
+
+        ratio = _calculateColorRatio(red, total)
+        rg = _calculateColorRatio(red, green)
+        rb = _calculateColorRatio(red, blue)
+
+        if ratio > highestR and (rg > 1.1 or rb > 1.1) and red > 20 and red > green and red > blue:
+          # So it's got a high R, but the G and B need to be far away.
+          highestR = ratio
+          location = (row, column)
+          print "Ratio %f, RG %f, RB %f" % (highestR, rg, rb)
+          print "Red %d, %d" % location
+
+    x, y = location
+    print "Final %d, %d" % (x, y)
+    print image[x][y]
+
+  # UTILITY FUNCTIONS
+
+  def _getDimensions(self, image, dimension="both"):
+    if dimension == "rows":
+      return len(image)
+    elif dimension == "columns":
+      return len(image[0])
+    else:
+      return (len(image), len(image[0]))
+
+  def _calculateColorRatio(self, a, b):
+    if b > 0:
+      return float(a) / float(b)
+    elif a > 0:
+      return 1
+    else:
+      return 0
+
+  def _isSimilar(self, a, b, sensitivity=20):
+    if ((a[0] > b[0] - sensitivity / 2 and a[0] < b[0] + sensitivity / 2) and
+        (a[1] > b[1] - sensitivity / 2 and a[1] < b[1] + sensitivity / 2) and
+        (a[2] > b[2] - sensitivity / 2 and a[2] < b[2] + sensitivity / 2)):
+      return True
+    else:
+      return False
+
   def __init__(self):
     """
     Videographer
