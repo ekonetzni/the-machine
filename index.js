a777a2b0d9293132f6a3f5e6a015b82ebd893916
Elliot Konetzni <ekonetzni@gmail.com>
2020-02-02 20:04:03 +0100

    initial on branch

---
+++ index.js
@@ -0,0 +1,17 @@
+
+/*
+ * Methods signature (currentTarget, args: {previousTargets: {step, target}, params, context, name })
+ */
+
+ /*
+  * - Retrieve feeds from RSS (feedparser) - Title
+  * - Dedupe against titles we've used - Title
+  * - Search and download video content based on title - FileName
+  * - Process video down to image - Image
+  * - Clean up
+  */
+
+const method1 = ({}, )
+const execute = async methods => initialParams => {
+    
+}
