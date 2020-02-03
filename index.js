6c95bc289dd75ea29b0dafb53bb840b08a362394
Elliot Konetzni <elliot.konetzni@booking.com>
2020-02-03 15:12:04 +0100

    control pane

--- index.js
+++ index.js
@@ -1,17 +1,73 @@
-
+const util = require('util');
 /*
- * Methods signature (currentTarget, args: {previousTargets: {step, target}, params, context, name })
+ * Methods signature (currentTarget, args: {previousTargets: [], params, context, name })
  */
 
- /*
-  * - Retrieve feeds from RSS (feedparser) - Title
-  * - Dedupe against titles we've used - Title
-  * - Search and download video content based on title - FileName
-  * - Process video down to image - Image
-  * - Clean up
-  */
+/*
+ * - Retrieve feeds from RSS (feedparser) - Title
+ * - Dedupe against titles we've used - Title
+ * - Search and download video content based on title - FileName
+ * - Process video down to image - Image
+ * - Clean up
+ */
 
-const method1 = ({}, )
-const execute = async methods => initialParams => {
-    
-}
+const _dump = obj => util.inspect(obj, { showHidden: false, depth: null });
+const _control = msg => console.log(`[Control] ${msg}`);
+// const getTitle = (
+//   currentTarget,
+//   // { previousTargets: [], params, context, name }
+//   args
+// ) => {
+//   const title = 'Donnie Trumpet';
+//   const result = { name: 'getTitle', result: title, args };
+//   console.log(`Returning from getTitle with ${_dump(result)} `);
+//   console.log('FUCKKKJAS', result);
+//   return result;
+// };
+
+const getTitle = (
+  currentTarget,
+  // { previousTargets: [], params, context, name }
+  args
+) => ({
+  name: 'getTitle',
+  result: 'donnie trumpet',
+  args
+});
+
+const downloadMaterial = (
+  currentTarget,
+  // { previousTargets: [], params, context, name }
+  args
+) => {
+  const video = { bunchOfData: ';ljasdlpkasjdasj' };
+  return { name: 'downloadMaterial', result: video, args };
+};
+
+const executor = (previousResult, currentMethod, index) => {
+  _control(`previousResult is ${_dump(previousResult)} at index ${index}`);
+  _control(`About to execute ${currentMethod.toString()}`);
+  const { result, args } = previousResult;
+  return currentMethod(result, {
+    previousTargets: [...args.previousTargets, result],
+    params: args.params,
+    context: args.context
+  });
+};
+
+const execute = methods => (initialTarget = {}) => {
+  const initialValue = {
+    result: initialTarget,
+    name: 'initial',
+    args: {
+      previousTargets: [],
+      params: {},
+      context: {}
+    }
+  };
+  const result = methods.reduce(executor, initialValue);
+  _control(`Result ${_dump(result)}`);
+};
+
+const constructVideo = execute([getTitle, downloadMaterial]);
+constructVideo({});
