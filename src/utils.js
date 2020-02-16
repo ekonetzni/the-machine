c6c35ed377815a377db12d5184a2675e687f5cc2
Elliot Konetzni <elliot@konetzni.com>
2020-02-16 20:15:07 +0100

    stashing a mock image

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,12 @@
 const util = require('util');
 const config = require('config');
+const fs = require('fs');
+
+const writeBlob = (path, arrayData) =>
+  fs.writeFileSync(path, JSON.stringify(arrayData));
 
 module.exports = {
   dump: obj => util.inspect(obj, { showHidden: false, depth: null }),
-  control: (msg, name = '') => console.log(`[Control] {${name}} ${msg}`)
+  control: (msg, name = '') => console.log(`[Control] {${name}} ${msg}`),
+  writeBlob
 };
