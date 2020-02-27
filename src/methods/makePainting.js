03b4f5a21b7c696eadb94f5df782e15c5f3b7f76
Elliot Konetzni <elliot.konetzni@booking.com>
2020-02-27 15:40:00 +0100

    moving code around

--- src/methods/makePainting.js
+++ src/methods/makePainting.js
@@ -5,115 +5,10 @@
  * }
  */
 const { control, getRandomInt, writeBlob } = require('../utils');
+const midlineHorizontalInterleaved = require('../modifiers/midlineHorizontalInterleavedGradient');
 
 const METHOD_NAME = 'makePainting';
 const _log = msg => control(msg, METHOD_NAME);
-
-const _generateFilledRow = (columns, colorValue) =>
-  Array.from({ length: columns }, () => colorValue);
-
-const _generateTriangularGradient = (
-  length,
-  colorA,
-  colorB,
-  gradient,
-  sizeFactor
-) => {
-  return Array.from({ length }, (_, i) => {
-    // Out of every sizeFactor pixels, gradient should be colorA
-    // the remainder should be colorB
-    return i % sizeFactor > gradient ? colorB : colorA;
-  });
-};
-
-const _midlineHorizontalTriangular = sizeFactor => target => {
-  const targetLength = target[0].length;
-  const samplePixelIndex = targetLength / 2; // midline
-
-  // Allocate a big ass array.
-  let outputRows = new Array(target.length * sizeFactor);
-  const outputColumns = targetLength * sizeFactor;
-  const baseRows = outputRows.length;
-  let row = 0;
-  let forwardColor;
-  let gradientFactor = 0;
-  for (row = 0; row < baseRows; row++) {
-    const targetRow = Math.floor(row / sizeFactor);
-
-    if (row % sizeFactor === 0) {
-      // Every nth row look forward n rows and
-      // snag a sample color
-      // set gradient factor to n and decrement by 1 each
-      // subsequent loop
-      forwardColor =
-        targetRow + sizeFactor < target.length - 1
-          ? target[targetRow + sizeFactor][samplePixelIndex]
-          : target[target.length - 1][samplePixelIndex];
-      gradientFactor = sizeFactor;
-    } else {
-      gradientFactor = gradientFactor - 1;
-    }
-
-    const color = target[targetRow][samplePixelIndex];
-    outputRows[row] = _generateTriangularGradient(
-      outputColumns,
-      color,
-      forwardColor,
-      gradientFactor,
-      sizeFactor
-    );
-  }
-
-  return outputRows;
-};
-
-const _generateInterleavedGradient = (length, colorA, colorB, gradient) =>
-  Array.from({ length }, (_, i) => (i % gradient === 0 ? colorB : colorA));
-
-const _midlineHorizontalInterleaved = sizeFactor => target => {
-  const targetLength = target[0].length;
-  const samplePixelIndex = targetLength / 2; // midline
-
-  // Allocate a big ass array.
-  let outputRows = new Array(target.length * sizeFactor);
-  const outputColumns = targetLength * sizeFactor;
-  const baseRows = outputRows.length;
-  let row = 0;
-  let forwardColor;
-  let gradientFactor = 0;
-  for (row = 0; row < baseRows; row++) {
-    const targetRow = Math.floor(row / sizeFactor);
-
-    if (row % sizeFactor === 0) {
-      // Every nth row look forward n rows and
-      // snag a sample color
-      // set gradient factor to n and decrement by 1 each
-      // subsequent loop
-      forwardColor =
-        targetRow + sizeFactor < target.length - 1
-          ? target[targetRow + sizeFactor][samplePixelIndex]
-          : target[target.length - 1][samplePixelIndex];
-      gradientFactor = sizeFactor + 2;
-    } else {
-      gradientFactor = gradientFactor - 1;
-    }
-
-    if (gradientFactor < 2) {
-      _log(
-        `gradientFactor is ${gradientFactor} (<2) at r ${row} and sf ${sizeFactor}`
-      );
-    }
-    const color = target[targetRow][samplePixelIndex];
-    outputRows[row] = _generateInterleavedGradient(
-      outputColumns,
-      color,
-      forwardColor,
-      gradientFactor
-    );
-  }
-
-  return outputRows;
-};
 
 const makePainting = async (currentTarget, args) => {
   const { sizeFactor } = args.context.settings;
@@ -123,7 +18,7 @@ const makePainting = async (currentTarget, args) => {
     _log(
       `Received target of dimensions ${currentTarget.length}, ${currentTarget[0].length}`
     );
-    currentTarget = _midlineHorizontalInterleaved(sizeFactor)(currentTarget);
+    currentTarget = midlineHorizontalInterleaved(sizeFactor)(currentTarget);
     _log(
       `After processing, target has dimensions ${currentTarget.length}, ${currentTarget[0].length}`
     );
