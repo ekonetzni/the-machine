ba559c1af923dbbca5c117b600d459f72f61fdda
Elliot Konetzni <elliot@konetzni.com>
2020-04-16 21:02:25 +0200

    sizeFactor comes from a different place

--- src/modifiers/midlineHorizontalAverage.js
+++ src/modifiers/midlineHorizontalAverage.js
@@ -3,12 +3,11 @@ const { control } = require('../utils');
 const METHOD_NAME = 'makePainting';
 const _log = msg => control(msg, METHOD_NAME);
 
-const _accumulateReducer = (acc, color) =>
-  [
-    acc[0] + Math.pow(color[0], 2),
-    acc[1] + Math.pow(color[1], 2),
-    acc[2] + Math.pow(color[2], 2)
-  ];
+const _accumulateReducer = (acc, color) => [
+  acc[0] + Math.pow(color[0], 2),
+  acc[1] + Math.pow(color[1], 2),
+  acc[2] + Math.pow(color[2], 2),
+];
 
 const _meanAbs = total => val => Math.abs(val / total);
 
@@ -16,16 +15,15 @@ const _sqrt = val => Math.sqrt(val);
 
 const _averaged = sample =>
   sample
-    .reduce(_accumulateReducer,
-      [0, 0, 0]
-    )
+    .reduce(_accumulateReducer, [0, 0, 0])
     .map(_meanAbs(sample.length))
     .map(_sqrt);
 
 const _gatherSamples = (target, index, rowsToSample, sampleColumnIndex) =>
   target.slice(index, index + rowsToSample).map(row => row[sampleColumnIndex]);
 
-const mhAverage = sizeFactor => original => {
+const mhAverage = (original, settings) => {
+  const { sizeFactor } = settings;
   const originalColumns = original[0].length;
   const originalRows = original.length;
   const samplePixelIndex = originalColumns / 2; // midline
